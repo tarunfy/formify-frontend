a2950517ce42a80b7f1139fcce863445c40461e3
tarun2506 <tarunsharma8920@gmail.com>
2022-01-05 21:34:22 +0530

    Fix URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://formify-backend.herokuapp.com",
+  baseURL: "https://formify-app.herokuapp.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
